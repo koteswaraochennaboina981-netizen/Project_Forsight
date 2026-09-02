9fca4c5be8c5d71e0575aeab0285d840458e9715
Koteswarao Chennaboina <koteswaraochennaboina981@gmail.com>
2026-09-02 09:56:10 +0530

    Initialize Streamlit app for Project Foresight

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Project Forsight started sucessfully")
+import streamlit as st
+import runpy
+
+st.set_page_config(
+    page_title="Project Foresight",
+    layout="wide"
+)
+
+runpy.run_path("ml_part/08_dashboard.py", run_name="__main__")
